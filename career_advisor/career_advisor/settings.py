ae78549f8ef9ab8b678c197ebc686c46d57e575f
sathwikadanthurii@gmail.com <sathwikadanthurii@gmail.com>
2026-03-30 14:44:58 +0530

    DB

--- career_advisor/career_advisor/settings.py
+++ career_advisor/career_advisor/settings.py
@@ -139,9 +139,15 @@ LOGIN_URL = '/login/'  # The URL for the login page
 import os
 import dj_database_url
 
-ALLOWED_HOSTS = ['*']
-
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
